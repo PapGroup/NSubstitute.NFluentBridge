fdfce5b4d9701fbc9b2e58debf4e4c3386e020a2
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2020-03-11 18:10:31 +0330

    summary for help class

--- code/src/PAP.NSubstitute.NFluentBridge/Help.cs
+++ code/src/PAP.NSubstitute.NFluentBridge/Help.cs
@@ -4,8 +4,17 @@ using NSubstitute.Core.Arguments;
 
 namespace PAP.NSubstitute.NFluentBridge
 {
+    /// <summary>
+    /// The class that integrates NSubstitute with NFluent
+    /// </summary>
     public static class Help
     {
+        /// <summary>
+        /// The method that verifies the arguments of called methods using NFluent
+        /// </summary>
+        /// <typeparam name="T">type of arguments to verify</typeparam>
+        /// <param name="check">delegate that contains assertion</param>
+        /// <returns>parameter of called method</returns>
         public static T Me<T>(Func<T, ICheckLink<ICheck<T>>> check)
         {
             return ArgumentMatcher.Enqueue(new NFluentMatcherAdapter<T>(check));
